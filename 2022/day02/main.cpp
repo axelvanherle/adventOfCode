aa9f59b3407be4d90bff21b085e84083da8e1301
Axel Vanherle <axelvanherle@outlook.com>
2022-12-02 07:53:43 +0100

    Update main.cpp

--- 2022/day02/main.cpp
+++ 2022/day02/main.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello, world!" << endl;
+	string line;
+	ifstream myfile("input.txt");
+
+	int score = 0;
+
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			if (line[0] == 'A')
+			{
+				if (line[2] == 'X')
+				{
+					cout << line << "test" << endl;
+				}
+				if (line[2] == 'Y')
+				{
+					cout << line << "test" << endl;
+				}
+				if (line[2] == 'Z')
+				{
+					cout << line << "test" << endl;
+				}
+			}
+			else if (line[0] == 'B')
+			{
+				if (line[2] == 'X')
+				{
+					cout << line << "test" << endl;
+				}
+				if (line[2] == 'Y')
+				{
+					cout << line << "test" << endl;
+				}
+				if (line[2] == 'Z')
+				{
+					cout << line << "test" << endl;
+				}
+			}
+			else if (line[0] == 'C')
+			{
+				if (line[2] == 'X')
+				{
+					cout << line << "test" << endl;
+				}
+				if (line[2] == 'Y')
+				{
+					cout << line << "test" << endl;
+				}
+				if (line[2] == 'Z')
+				{
+					cout << line << "test" << endl;
+				}
+			}
+			
+		}
+		myfile.close();
+	}
+
+	else
+	{
+		cout << "Unable to open file";
+	}
+
+	return 0;
 }
