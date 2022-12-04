dd6008dc81a2aaab4c4796b108935eb94deb0429
Axel Vanherle <axelvanherle@outlook.com>
2022-12-04 09:51:00 +0100

    Finished day4

---
+++ 2022/day04/part2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	string line;
+	ifstream myfile("input.txt");
+
+	int c = 0;
+	int num1Min, num1Max, num2Min, num2Max;
+	char trash;
+
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			myfile >> num1Min >> trash >> num1Max >> trash >> num2Min >> trash >> num2Max;
+			cout << num1Min << trash << num1Max << trash << num2Min << trash << num2Max << endl;
+
+			if(num1Min > num2Max || num2Min > num1Max)
+			{
+
+			}
+			else
+			{
+				c++;
+
+				// ;)
+			}
+		}
+
+		myfile.close();
+	}
+
+	else
+	{
+		cout << "Unable to open file";
+	}
+
+	cout << c << endl;
+}
